b0095b5b2d79100eebd75ebdcaa1d2946133b051
FreddyLiu06 <103144599+FreddyLiu06@users.noreply.github.com>
2022-09-09 23:04:25 -0400

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,61 @@
+import axios from 'axios';
+import config from './config';
+
+
+// API call to search for a book given name and title
+const search = async (title, author) => {
+    const maxResults = 40; // Maximum number of book results to be returned by the API after each search, max num is 40
+    if (title) {
+        title = title.replace(/\s/g, '+'); // Replace spaces with + for search
+    } else {
+        title = '';
+    }
+
+    if (author) {
+        author = author.replace(/\s/g, '+');
+        author = `inauthor:${author}`
+    } else {
+        author = '';
+    }
+    const result = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${title}+${author}&key=${config.booksApiKey}&maxResults=${maxResults}`);
+    return result;
+}
+
+// Method to search for a book given the ID
+const getBookByID = async (bookID) => {
+    const result = await axios.get(`https://www.googleapis.com/books/v1/volumes/${bookID}?key=${config.booksApiKey}`);
+    return result;
+}
+
+// Add a book to bookshelf
+const addBookToLibrary = async (bookID, title, authors, publishDate, imageLink, genres) => {
+    const result = await axios({
+        method: 'post',
+        url: 'http://127.0.0.1:8000/api/addBookToLibrary/',
+        data: JSON.stringify({
+            id: bookID,
+            title: title,
+            authors: authors,
+            publishDate: publishDate,
+            imageLink: imageLink,
+            genres: genres
+        }),
+        
+    })
+    return result;
+}
+
+// Get all books in the bookshelf
+const getBooksInShelf = async () => {
+    const result = await axios.get('http://127.0.0.1:8000/api/getBooksInShelf');
+    return result;
+}
+
+const api = {
+    search,
+    getBookByID,
+    addBookToLibrary,
+    getBooksInShelf,
+}
+
+export default api;
---
+++ src/pages/BookDetails.js
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from 'react'
+import api from '../api';
+import { Grid, Button, Card, CardContent, alertClasses} from "@mui/material";
+import { Search } from "@mui/icons-material";
+
+const BookDetails = (props) => {
+    // Get ID of book being displayed
+    const bookID = props.location.state.bookID;
+
+    const [bookData, setBookData] = useState({});
+
+    const getAuthorNames = () => {
+        let authors = '';
+        bookData.volumeInfo.authors.forEach((author) => {
+            authors = `${author}, `
+        })
+        return authors.slice(0,-2);
+    }
+
+    // Initial method to get the book data from the api
+    const init = async () => {
+        const result = await api.getBookByID(bookID);
+        setBookData(result.data);
+    }
+
+    useEffect(() => {
+        init();
+    }, [])
+
+    return (
+        <React.Fragment>
+            <Grid container spacing={2} justifyContent='center'>
+                <Grid item xs = {8.5} spacing={2} justifyContent='center'>
+                    {Object.keys(bookData).length > 0 ?
+                    <Card>
+                        <CardContent>
+                            <Grid container spacing={1} justifyContent='center'>
+                                <Grid container item xs = {2} justifyContent='center'>
+                                    <Grid item style = {{marginBottom: 2}}>
+                                        <img src={bookData.volumeInfo?.imageLinks?.thumbnail}/>
+                                    </Grid>
+                                    <Grid item>
+                                        <Button>Add to bookshelf</Button>
+                                    </Grid>
+
+                                </Grid>
+                                <Grid item xs = {10}>
+                                    <h1>{bookData.volumeInfo.title}</h1>
+                                    <h2 style={{fontWeight: 'normal'}}>{getAuthorNames()}</h2>
+                                    <a style={{display:'inline'}} href={bookData.volumeInfo.previewLink}>Google Books</a>
+                                    <p style={{color: 'grey', fontSize: 10}}>Published {bookData.volumeInfo.publishedDate}</p>
+                                </Grid>
+                            </Grid>
+                        </CardContent>
+                    </Card> : 'LOADING'
+                    }
+                </Grid>
+            </Grid>
+        </React.Fragment>
+     )
+}
+
+export default BookDetails
